4d6af79aebf89ee7bea499d774a019afedcdd859
tochinicky <tochinicky@gmail.com>
2019-09-15 00:11:57 +0100

    CQRS and MediatR implementation with api logic

--- SocialApp/API/Startup.cs
+++ SocialApp/API/Startup.cs
@@ -2,6 +2,8 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Application.Activities;
+using MediatR;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
@@ -30,6 +32,7 @@ namespace API
             services.AddDbContext<DataContext>(opt => {
                 opt.UseSqlite(Configuration.GetConnectionString("DefaultConnection"));
             });
+            services.AddMediatR(typeof(List.Handler).Assembly);
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
         }
 
---
+++ SocialApp/Application/Activities/Edit.cs
@@ -0,0 +1,54 @@
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+using MediatR;
+using Persistance;
+
+namespace Application.Activities
+{
+    public class Edit
+    {
+        public class Command : IRequest
+        {
+            public Guid Id { get; set; }
+            public string Title { get; set; }
+            public string Description { get; set; }
+            public string Category { get; set; }
+            public DateTime? Date { get; set; }
+            public string City { get; set; }
+            public string Venue { get; set; }      
+        }
+        
+        public class Handler : IRequestHandler<Command>
+        {
+            private readonly DataContext _context;
+            public Handler(DataContext context)
+            {
+                        _context = context;
+            }
+        
+            public async Task<Unit> Handle(Command request, CancellationToken cancellationToken)
+            {
+                        //handler logic
+               var activities = await _context.Activities.FindAsync(request.Id);
+               if(activities == null)
+                 throw new Exception("Content not found");
+                 
+                activities.Title = request.Title ?? activities.Title;
+                activities.Description = request.Description ?? activities.Description;
+                activities.Category = request.Category ?? activities.Category;
+                activities.Date = request.Date ?? activities.Date;
+                activities.City = request.City ?? activities.City;
+                activities.Venue = request.Venue ?? activities.Venue;
+
+                var success =await _context.SaveChangesAsync() > 0;
+                if(success) return Unit.Value;
+        
+                throw new Exception("Problem saving to database");
+                  
+            }
+
+
+        }
+    }
+}
